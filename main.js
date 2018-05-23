3a60354f9d790b779f12458fdebbf01766f94718
Eric Engoron <eric.engoron@gmail.com>
2018-05-22 22:12:38 -0400

    Initial commit

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const { app, Menu, BrowserWindow } = require('electron')
 
 const path = require('path')
 const url = require('url')
@@ -13,7 +9,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600, tabbingIdentifier:"default"});
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -32,12 +28,39 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+  return mainWindow
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function () {
+  const template = [
+  ]
+  
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        {
+          label: 'New Tab',
+          accelerator: 'CmdOrCtrl+T',
+          click () {
+            const win = BrowserWindow.getFocusedWindow();
+            if (win) {
+              win2 = createWindow()
+              win.addTabbedWindow(win2)
+            }
+          }
+        }
+      ]
+    })
+  }
+  
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+  createWindow()
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -56,5 +79,9 @@ app.on('activate', function () {
   }
 })
 
+app.on('new-window-for-tab', createWindow)
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
